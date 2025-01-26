10a41d3b64745a40ca5e5252467bf35e9f38a88e
Zack Githui <jackszack177@gmail.com>
2025-01-26 15:51:45 +0300

    updates

--- app/src/main/java/com/example/cleanshelf/presentation/authentication/signIn/SignInEvents.kt
+++ app/src/main/java/com/example/cleanshelf/presentation/authentication/signIn/SignInEvents.kt
@@ -1,4 +1,11 @@
 package com.example.cleanshelf.presentation.authentication.signIn
 
-class SignInEvents {
+sealed class SignInEvents {
+    data class EmailChanged(val email:String):SignInEvents()
+    data class PasswordChanged(val password:String):SignInEvents()
+    data object ViewPassword:SignInEvents()
+    data object SignUpLabelClicked:SignInEvents()
+    data object SignInButtonClicked:SignInEvents()
+    data object ForgotLabelClicked:SignInEvents()
+
 }
