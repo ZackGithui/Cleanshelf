980718c4de6dbcb516bde31ef3adfcc0b0ddf0b6
Zack Githui <jackszack177@gmail.com>
2025-01-26 22:18:00 +0300

    Forgot password

---
+++ app/src/main/java/com/example/cleanshelf/presentation/authentication/forgotPassword/ForgotState.kt
@@ -0,0 +1,3 @@
+package com.example.cleanshelf.presentation.authentication.forgotPassword
+
+data class ForgotState()
---
+++ app/src/main/java/com/example/cleanshelf/presentation/authentication/forgotPassword/ForgotViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.cleanshelf.presentation.authentication.forgotPassword
+
+class ForgotViewModel {
+}
